9e4051d34b8409916758f04683bc991c042194b8
Leon Kote <roman892265@gmail.com>
2025-05-05 21:28:36 +0500

    Added InfoController

---
+++ Api/Controllers/InfoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Route("api/info")]
+public class InfoController : ControllerBase
+{
+	[HttpGet]
+	public IActionResult GetInfo()
+	{
+		return Ok(new { result = Environment.MachineName });
+	}
+}
